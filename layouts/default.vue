aa58f96c02a457f496935176ca9d5e9b3ab8964f
kaito.tada <kuyt1992@gmail.com>
2021-10-10 00:07:59 +0900

    feat: page/indexのlayout作成(wip)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <Header />
-    <main class="flex-grow justify-center items-center m-auto flex-col max-w-screen-lg">
+    <main class="container flex-grow mx-auto px-6 py-8 max-w-screen-lg">
       <nuxt />
     </main>
     <Footer />
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <ul>
-    <li v-for="content in contents" :key="content.id">
-      <nuxt-link :to="`/${content.id}`">
-        {{ content.title }}
-      </nuxt-link>
-    </li>
-  </ul>
+  <div class="container flex justify-between mx-auto">
+    <div class="w-full lg:w-8/12">
+      <div class="px-10 py-6 mx-auto bg-white border rounded-sm shadow-sm">
+        <ul>
+          <li v-for="content in contents" :key="content.id">
+            <nuxt-link :to="`/${content.id}`">
+              {{ content.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="w-full lg:w-4/12 -mx-8 flex-col md:flex-row">
+      <div class="flex flex-col max-w-sm px-6 py-4 mx-auto bg-white border rounded-sm shadow-sm">
+        <p>プロフィールカード</p>
+      </div>
+      <div class="flex flex-col max-w-sm px-6 py-4 mx-auto bg-white border rounded-sm shadow-sm">
+        <p>Twitter</p>
+      </div>
+      <div class="flex flex-col max-w-sm px-6 py-4 mx-auto bg-white border rounded-sm shadow-sm">
+        <p>カテゴリー</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
