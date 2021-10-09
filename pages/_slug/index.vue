91e2056ad4965a1ab99f38f42ed7720ac0b6a781
kaito.tada <kuyt1992@gmail.com>
2021-10-09 17:47:39 +0900

    initial commit

---
+++ pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData ({ params }) {
+    const { data } = await axios.get(
+      `https://${process.env.SERVICE_DOMAIN}.microcms.io/api/v1/blog/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': process.env.API_KEY }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    const { data } = await axios.get(
+      `https://${process.env.SERVICE_DOMAIN}.microcms.io/api/v1/blog`,
+      {
+        headers: { 'X-API-KEY': process.env.API_KEY }
+      }
+    )
+    return data
+  }
+}
+</script>
