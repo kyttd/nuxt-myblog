2eceb6a35e94c8111c022eada96efe07dd2957bf
kaito.tada <kuyt1992@gmail.com>
2021-10-09 22:54:21 +0900

    feat: Base Layout作成

---
+++ components/commons/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="flex py-5 px-3 m-auto border-b max-w-6xl">
+      <div class="flex items-center text-gray-800">
+        <span class="font-semibold text-xl tracking-tight">hal_sea_ / blog</span>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <main class="flex flex-1 justify-center items-center m-auto flex-col max-w-6xl min-h-screen">
+      <nuxt />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '~/components/commons/Header.vue'
+import Footer from '~/components/commons/Footer.vue'
+
+export default Vue.extend({
+  components: {
+    Header,
+    Footer
+  }
+})
+</script>
