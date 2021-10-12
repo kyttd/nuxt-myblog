6c4069ddc1d5ab8662e846fa43cc02ebc7219215
kaito.tada <kuyt1992@gmail.com>
2021-10-12 22:59:48 +0900

    feat: Headerにメニュー追加

--- components/commons/Header.vue
+++ components/commons/Header.vue
@@ -1,8 +1,24 @@
 <template>
   <header>
     <div class="flex py-5 px-4 m-auto border-b max-w-screen-lg">
-      <div class="flex items-center">
-        <span class="font-semibold text-xl tracking-tight">hal_sea_ / blog</span>
+      <div class="flex items-center mr-6">
+        <nuxt-link :to="'/'">
+          <span class="font-semibold text-xl tracking-tight">
+            hal_sea_ / blog
+          </span>
+        </nuxt-link>
+      </div>
+      <div class="flex items-center px-4">
+        <nuxt-link :to="'/about'">
+          <span class="font-sm mr-4">
+            about
+          </span>
+        </nuxt-link>
+        <nuxt-link :to="'/profile'">
+          <span class="font-sm mr-4">
+            profile
+          </span>
+        </nuxt-link>
       </div>
     </div>
   </header>
@@ -10,7 +26,5 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
-  
-})
+export default Vue.extend({})
 </script>
