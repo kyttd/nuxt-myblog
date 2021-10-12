36a9a4c2c06d6f0c56a01de1cf233bcf445e74f3
kaito.tada <kuyt1992@gmail.com>
2021-10-12 23:00:25 +0900

    feat: about,profileページ追加

---
+++ pages/about/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-wrap items-start w-full sm:w-3/4 m-auto">
+    <div class="flex justify-center items-center border-b">
+      <h1 class="text-xl font-bold mb-2">
+        このブログについて
+      </h1>
+    </div>
+    <div class="w-full py-4 px-2">
+      <p>
+        主にプログラミング関連のアウトプットに使います(予定)。
+        <br>
+        あくまで予定なのでポエムっぽいものも書くかもしれないし、趣味について書くかもしれないし、ただの日記を書くかもしれません。
+      </p>
+      <h2 class="text-lg mt-4 font-bold">
+        技術構成
+      </h2>
+      <p>このブログの技術構成はこんな感じです。</p>
+      <ul class="list-disc px-6 my-2">
+        <li>Nuxt.js</li>
+        <li>TypeScript</li>
+        <li>microCMS</li>
+        <li>Tailwindcss</li>
+        <li>Netlify</li>
+      </ul>
+      <h2 class="text-lg mt-4 font-bold">
+        ソースコード
+      </h2>
+      <p>GitHubに上げてますのでご参考まで</p>
+      <a class="underline" href="https://github.com/kyttd/nuxt-myblog" target="_blank" rel="noopener noreferrer">
+        https://github.com/kyttd/nuxt-myblog
+      </a>
+    </div>
+  </div>
+</template>
---
+++ pages/profile/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex flex-wrap items-start w-full sm:w-3/4 m-auto">
+    <div class="flex justify-center items-center border-b">
+      <h1 class="text-xl font-bold mb-2">
+        書いてる人
+      </h1>
+    </div>
+    <div class="w-full pt-4 px-2">
+      <p>ハル(@hal_sea_)と申します。都内でWebエンジニアをやっています。1992年生まれの男です。</p>
+      <h2 class="text-lg mt-4 font-bold">
+        経歴
+      </h2>
+      <ul class="list-disc px-6 mb-2">
+        <li>1992年9月 東京生まれ</li>
+        <li>2016年4月〜 大学卒業後、金融系の会社で法人営業を4年ほど</li>
+        <li>2020年4月〜 受託/SES系企業でエンジニアデビュー</li>
+        <li>2021年9月〜 住まい系の自社サービス企業に転職</li>
+      </ul>
+      <p>現在は主にNuxt.jsでフロントエンド周りを触ることが多いです。個人開発でNext.js/React触ったりもしてます。まだまだ修行中の身。</p>
+      <p>エンジニア一年経った時にこんな記事をQiitaに書きました。</p>
+      <a class="underline" href="https://qiita.com/hal_sea_/items/03284efc23e9c769c7f8" target="_blank" rel="noopener noreferrer">
+        実務未経験でエンジニア転職してから1年経過した話（Qiita）
+      </a>
+      <h2 class="text-lg mt-4 font-bold">
+        業務で使用したことのある技術
+      </h2>
+      <p>Ruby on Rails, Laravel, Python, Zend Framework, React, Vue.js, Nuxt.js, JavaScript, TypeScript, jQuery, AWS, Docker etc… </p>
+      <p>浅く広く色々触ってます。当面はJS/TS辺りを鍛えてフロントエンド周り強くなりたいと思ってます。</p>
+      <h2 class="text-lg mt-4 font-bold">
+        好きなもの
+      </h2>
+      <ul class="list-disc px-6 mb-2">
+        <li>
+          音楽/映画
+          <ul>
+            <li>オルタナ系の音楽とかA24系の映画とか好きです。</li>
+          </ul>
+        </li>
+        <li>
+          お笑い
+          <ul>
+            <li>空気階段が好きです。祝キングオブコントチャンピオン。</li>
+          </ul>
+        </li>
+        <li>
+          サウナ
+          <ul>
+            <li>高温多湿で空いてる所が好きです。（あんまりないですが…）</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
