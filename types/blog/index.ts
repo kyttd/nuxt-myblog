a1aa9dd2829f404815d4facd27638b1615765063
kaito.tada <kuyt1992@gmail.com>
2021-10-19 23:54:57 +0900

    feat: カテゴリーとタグの型定義追加

--- types/blog/index.ts
+++ types/blog/index.ts
@@ -1,8 +1,18 @@
 import { CommonItem, CommonList } from '../api'
 
+export type CategoryItem = CommonItem & {
+  name: string
+}
+
+export type TagItem = CommonItem & {
+  name: string
+}
+
 export type BlogItem = CommonItem & {
   title: string
   body: string
+  category: CategoryItem
+  tags: TagItem[]
 }
 
 export type BlogIndexApiResponse = CommonList<BlogItem>
