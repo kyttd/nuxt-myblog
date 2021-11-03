7efbeaa026aa33a1c2aa404d0021c03a76010699
kaito.tada <kuyt1992@gmail.com>
2021-11-03 16:59:13 +0900

    feat: HeadingWithIconコンポーネント作成

---
+++ components/atoms/HeadingWithIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2 class="bg-blue-100 p-2 bg-opacity-50 border-0 rounded">
+    <unicon class="align-middle" :name="uniconName" fill="royalblue" />
+    <span class="align-middle text-lg font-bold">{{ title }}</span>
+  </h2>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    uniconName: { type: String, required: true },
+    title: { type: String, required: true }
+  }
+})
+</script>
