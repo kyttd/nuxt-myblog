d6fe539c2c62b0cf39bd38ae46f91d58fbf01eae
kaito.tada <kuyt1992@gmail.com>
2021-11-05 21:41:40 +0900

    update: HeadingWithIconのスタイル修正

--- components/atoms/HeadingWithIcon.vue
+++ components/atoms/HeadingWithIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2 class="bg-blue-100 p-2 bg-opacity-50 border-0 rounded">
+  <h2 class="bg-blue-100 p-2 w-full bg-opacity-50 border-0 rounded">
     <unicon class="align-middle" :name="uniconName" fill="royalblue" />
     <span class="align-middle text-lg font-bold">{{ title }}</span>
   </h2>
