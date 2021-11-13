f14c1dd347875bd73e287aa5625e304706c5f4cc
kaito.tada <kuyt1992@gmail.com>
2021-11-13 16:53:56 +0900

    feat: 目次にスタイル適用

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -29,10 +29,18 @@
       class="mb-4 px-4 py-2 w-full bg-gray-100 border rounded"
     >
       <ul ref="toc" class="toc divide-y">
-        <li class="py-2 font-bold">目次</li>
+        <li class="py-2 font-bold">
+          <unicon
+            class="align-middle mr-1"
+            width="18"
+            height="18"
+            name="list-ul"
+            fill="royalblue"
+          />目次
+        </li>
         <li v-for="item in toc" :key="item.id" class="py-2">
           <nuxt-link v-scroll-to="`#${item.id}`" to>
-            {{ item.text }}
+            <span :class="tocStyle(item.name)">- {{ item.text }}</span>
           </nuxt-link>
         </li>
       </ul>
@@ -130,6 +138,18 @@ export default Vue.extend({
     },
     isShowToc(): boolean {
       return !!this.toc.length
+    },
+    tocStyle() {
+      return function (name: string): string | undefined {
+        switch (name) {
+          case 'h2':
+            return 'ml-2 font-semibold'
+          case 'h3':
+            return 'ml-4'
+          case 'h4':
+            return 'ml-6'
+        }
+      }
     }
   }
 })
