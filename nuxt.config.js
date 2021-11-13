e13507bf09e4bb5f91feed100fb9cad2def9a88c
kaito.tada <kuyt1992@gmail.com>
2021-11-13 17:36:18 +0900

    change: plugin.jsをunicon.jsにファイル名変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '@/plugins/vue-scrollto' },
-    { src: '@/plugins/plugin', mode: 'client' }
+    { src: '@/plugins/unicon', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
